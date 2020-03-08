8d1181f5ad4c9a7a98ae2dc8d0428325d44b9b1a
albi23 <piekielny7891>
2020-03-08 11:24:06 +0100

    2 => 1

--- LaboratoryFirst/src/main/java/DataCollectorTest.java
+++ LaboratoryFirst/src/main/java/DataCollectorTest.java
@@ -6,7 +6,7 @@ public class DataCollectorTest {
 
         DataTestReader dataTestReader = (args.length > 0) ? new DataTestReader(args[0]) : new DataTestReader();
         final Map<Byte, DataCollector> symbolsData = dataTestReader.getSymbolsData();
-        if (args.length > 1 && args[2].equals("-t")) dataTestReader.printData(symbolsData);
+        if (args.length > 1 && args[1].equals("-t")) dataTestReader.printData(symbolsData);
         System.out.println("Entropy             : " + String.format("%f", dataTestReader.countEntropy(symbolsData)));
         System.out.println("Conditional Entropy : " + String.format("%f", dataTestReader.countConditionalEntropy(symbolsData)));
     }
